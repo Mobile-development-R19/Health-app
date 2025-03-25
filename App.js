ca8a9860a2742ad0db54e5e710da2c2b0696e4a2
vsuho <156258471+vsuho@users.noreply.github.com>
2025-03-25 22:50:57 +0200

    Sign up and login

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Signup from './screens/Signup';
+import Login from './screens/Login';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator()
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Signup' screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Signup' component={Signup} />
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='HomeScreen' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ firebase/Config.js
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, addDoc, query, onSnapshot, deleteDoc, doc, setDoc } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const database = getDatabase(app)
+
+export {
+    auth, 
+    database,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    onSnapshot,
+    deleteDoc,
+    doc,
+    setDoc
+    
+}
