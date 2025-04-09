321ebe21b2d459e6476daed853773395f5383bbb
Samulimv <t3vasa03@students.oamk.fi>
2025-04-09 13:10:51 +0300

    service file

--- frontend/firebase/Config.js
+++ frontend/firebase/Config.js
@@ -6,12 +6,12 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID
+  apiKey: 'AIzaSyB17tM7VnPWMel5HdVfo4B85lv-GrR7YmI',
+  authDomain: 'sports-app-c21d2.firebaseapp.com',
+  projectId: 'sports-app-c21d2',
+  storageBucket: 'sports-app-c21d2.firebasestorage.app',
+  messagingSenderId: '97763493955',
+  appId: '1:97763493955:web:c458b43ad7dbad37e16cf6',
 };
 
 // Initialize Firebase
